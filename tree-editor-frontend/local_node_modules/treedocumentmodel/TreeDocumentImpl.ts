7874d524b847b83fd5b7d842d54fb65aeff4190d
Kristian Rekstad <krirek@msn.com>
2021-03-25 13:59:11 +0100

    Remove local_node_modules

--- tree-editor-frontend/local_node_modules/treedocumentmodel/TreeDocumentImpl.ts
+++
@@ -1,8 +0,0 @@
-import {TreeDocument} from "./TreeDocument";
-import {TreeDocumentBase} from "./TreeDocumentBase";
-import {Set} from "crossecore/lib/Set";
-export class TreeDocumentImpl
-extends TreeDocumentBase
-{
-	//implement your generated class here
-}
